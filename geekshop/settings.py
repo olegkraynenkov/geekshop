b9885081ca2921035a9fac0934ed9a5d5521ad68
olegkraynenkov <kraynenkov.o@gmail.com>
2021-01-21 09:28:33 +0300

    lesson1

--- geekshop/settings.py
+++ geekshop/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'rvfq=t&$o^$i(=xivybhl-6pt!foa6_c@clj9sdg8&swq3w!))'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'mainapp',
 ]
 
 MIDDLEWARE = [
@@ -118,3 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
